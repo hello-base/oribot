fb8434856cab10e39305759641634640672145c7
Bryan Veloso <bryan@github.com>
2012-07-15 12:15:12 -0700

    Skeleton models.

---
+++ orialy/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Release(models.Model):
+    pass
+
+
+class Daily(models.Model):
+    pass
+
+
+class Weekly(models.Model):
+    pass
+
+
+class Yearly(models.Model):
+    pass
