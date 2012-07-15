45abe29e1a59a67112d553b92ec59df2983c5115
Bryan Veloso <bryan@github.com>
2012-07-15 14:24:52 -0700

    Adding a utf-8 prelude.

---
+++ orialy/settings/common.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+from os.path import abspath, basename, dirname, join, normpath
+from sys import path
+
--- orialy/settings/production.py
+++ orialy/settings/production.py
@@ -1,1 +1,5 @@
+# -*- coding: utf-8 -*-
 from common import *
+from dj_database_url import config
+
+DATABASES = {'default': config(default='postgres://localhost')}
