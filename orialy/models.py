be82118fbe53db1780e754a287ed77d22af786ec
Bryan Veloso <bryan@github.com>
2012-07-15 12:26:47 -0700

    Entry should be an abstract base model.

--- orialy/models.py
+++ orialy/models.py
@@ -1,13 +1,18 @@
 from django.db import models
+from django.db.models import Sum
 
 
 class Release(models.Model):
-    pass
+    def daily_sales(self):
+        return self.dailies.annotate(total=Sum('sales'))
 
 
 class Entry(models.Model):
     sales = models.IntegerField(blank=True, null=True)
     rank = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        abstract = True
 
 
 class Daily(Entry):
